c57274d28540d6f27a13d785beddf132741cf3c6
leet5 <leet5@proton.me>
2024-06-15 01:52:01 +0300

    feat: customer validation rules

--- src/main/java/com/leet5/ecommerce/model/entity/Customer.java
+++ src/main/java/com/leet5/ecommerce/model/entity/Customer.java
@@ -1,6 +1,9 @@
 package com.leet5.ecommerce.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,42 +15,70 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @NotBlank
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Email
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "birthdate")
     private LocalDate birthdate;
 
+    @Valid
     @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Order> orders;
+
+    public Long getId() {
+        return id;
+    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setFirstName(String firstName) {
+    public @NotBlank String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NotBlank String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName) {
+    public @NotBlank String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotBlank String lastName) {
         this.lastName = lastName;
     }
 
-    public void setEmail(String email) {
+    public @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email String email) {
         this.email = email;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
     }
 
     public void setBirthdate(LocalDate birthdate) {
         this.birthdate = birthdate;
     }
 
-    public void setOrders(List<Order> orders) {
+    public @Valid List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(@Valid List<Order> orders) {
         this.orders = orders;
     }
 }
